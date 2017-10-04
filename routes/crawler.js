8e71b723970628fab1937986df8ac8a172478ffa
Christian Ankert <christian.ankert@netpioneer.de>
2017-10-04 19:35:06 +0200

    Crawler working!

--- routes/crawler.js
+++ routes/crawler.js
@@ -3,12 +3,18 @@ var router = express.Router();
 var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
+var responseCode = '';
+var responseTime = '';
 
 router.get('/scrape/', function(req,res){
     //var pageToVisit = req.data;
 
     console.log('1. /scrape got called, starting Crawl');
     var websiteToCrawl = req.query.website;
+    var entryToUpdate = req.query.id;
+    var db = req.db;
+    var collection = db.get('websitelist');
+
     console.log('2. Starting Crawl for this Website: ' + websiteToCrawl);
 
     function getStatusCode(){
@@ -16,27 +22,40 @@ router.get('/scrape/', function(req,res){
             url: websiteToCrawl,
             time: true
         },function(err,response){
+            responseCode = response.statusCode;
+            responseTime = response.elapsedTime;
+
             console.log('3. Visiting Website ' + websiteToCrawl);
-            console.log('4. Status code of Website: ' + response.statusCode);
-            console.log('5. Response Time: ' + response.elapsedTime + 'ms');
+            console.log('4. Status code of Website: ' + responseCode);
+            console.log('5. Response Time: ' + responseTime + 'ms');
+
         });
     }
 
-    function updateDbEntry(req,res){
-        console.log('Starting db update');
-        var db = req.db;
-        var collection = db.get('websitelist');
-        var entryToUpdate = req.query.id;
-        console.log(req);
-        collection.updateOne(
-            {'_id' : entryToUpdate},
-            {$set: {'status': response.statusCode, 'responsetime':response.elapsedTime}},
-            function(err, result){
-            res.send(
-                (err === null) ? {msg: '' } : {msg: err }
-            );
-        });
+    function updateDbEntry(){
+        console.log('6. Starting db update for "' + entryToUpdate + '"');
+
+        collection.update(
+            { "_id" : entryToUpdate },
+            { "$set" : { 'status' : responseCode, 'responsetime' : responseTime } },
+            { "upsert" : true }
+        );
+        console.log('7. Success on Update');
+
     }
+
+        /*
+        collection.updateOne(myquery, newvalues, function(err, result){
+            if (err) throw err;
+            console.log("1 document updated");
+        });
+
+
+        collection.find({},{},function(e,docs){
+            console.log(docs);
+        });
+
+        */
 
 
     getStatusCode();
@@ -57,8 +76,6 @@ router.get('/scrape/', function(req,res){
     });
 });
 */
-
-
 
 
 module.exports = router;
