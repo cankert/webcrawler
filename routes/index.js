e0be4da67c380602353ee0a80a93dfc95402d76c
Christian Ankert <christian.ankert@netpioneer.de>
2017-10-10 15:22:25 +0200

    ..

--- routes/index.js
+++ routes/index.js
@@ -12,17 +12,18 @@ router.get('/', function(req, res, next) {
     });
 });
 
-router.get('/website/:id', function(req, res, next) {
+router.get('/website/:id/:name?', function(req, res, next) {
     var db = req.db;
     var collection = db.get('crawl');
     var documentId = req.params.id;
+    var name = req.params.name;
     //var ObjectId = ObjectId(documentId);
     //var query = {'websiteid': documentId};
     //console.log(query);
 
     collection.find({'websiteid': monk.id(documentId)}, {sort: {date:-1}}).then((website) => {
         //console.log(website);
-        res.render('website', { title: 'Test' , statusdata: website});
+        res.render('website', { title: name , statusdata: website});
     });
 });
 
